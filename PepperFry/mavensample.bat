31c6878966bd8134bb0727cb4da7d47ae4e4ba63
AkilaBV <akilabv@gmail.com>
2021-09-26 23:51:43 -0500

    update commited

--- PepperFry/mavensample.bat
+++ PepperFry/mavensample.bat
@@ -1,2 +1,2 @@
 cd C:\Users\vkche\Downloads\PepperFry\487_PepperyFry\PepperFry
-mvn clean install
+cmd /k mvn clean install
--- PepperFry/sample.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\vkche\Downloads\PepperFry\487_PepperyFry
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
